ea02e7a394d26253d018b10aa9afa3ce41864ef3
Sutaphat (Mew) <sutaphat@micware-ap.com>
2026-04-08 15:13:53 +0700

    Add Tailwind CSS framework  and PrimeVue for design layout.

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(PrimeVue, { theme: { preset: Aura } })
+  .mount('#app')
